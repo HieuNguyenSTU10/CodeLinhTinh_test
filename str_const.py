a356529d8067112c83643a632267f12c5b2c54eb
Ubuntu <azureuser@dataColection2.th3sahq3vdyu1axzhcxmgejwsh.syx.internal.cloudapp.net>
2024-09-01 16:20:09 +0700

    push data2

--- str_const.py
+++ str_const.py
@@ -6,7 +6,7 @@ test = True
 def khoi_tao_chrome():
     chrome_options = Options()
     # Đường dẫn đến thư mục chứa profile
-    profile_path = f"~\\CodeLinhTinh\\tk"
+    profile_path = f"tk"
     chrome_options.add_argument(f"user-data-dir={profile_path}")
     # Thêm thư mục profile vào các tùy chọn
     chrome_options.add_argument("--headless=new")
